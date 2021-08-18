8a33c77199f67570a15b07cceccbc16dc7953c86
刘炎培 <825639602@qq.com>
2021-08-18 09:37:11 +0800

    Update Monitor.py
    ss

--- Monitor.py
+++ Monitor.py
@@ -6,7 +6,7 @@ import sys
 
 
 def getJson():
-    url = "https://api.btc126.com/eth.php"
+    url = "https://eth.btc126.com/eth.php"
     response = requests.get(url)
     response.encoding = response.apparent_encoding
     dataarr = json.loads(response.text)
@@ -45,10 +45,10 @@ def tixing(argv):
     usdt = getBuy("USDT", dataArr)
     huoBi = getBuy("HUOBI", dataArr)
     rmb = huoBi*usdt
-    con = "���ETH��ǰ�۸�%sUSDT,ԼΪ%.2fRMB" % (huoBi, rmb)
+    con = "火币ETH当前价格：%sUSDT,约为%.2fRMB" % (huoBi, rmb)
     userInfoStr = argv[1]
-    from_addr = argv[2]  # �ʼ������˺�
-    qqCode = argv[3]  # ��Ȩ�루���Ҫ���Լ���ȡ���ģ�
+    from_addr = argv[2]  # 邮件发送账号
+    qqCode = argv[3]  # 授权码（这个要填自己获取到的）
     userInfo = userInfoStr.split("|")
     for user in userInfo:
         email = getValue(user, "email")
@@ -58,19 +58,19 @@ def tixing(argv):
         usdtlow = float(getValue(user, "usdtlow"))
         usdthigt = float(getValue(user, "usdthigt"))
         if rmbhigh != 0 and rmb > rmbhigh:
-            s = "<p><div style='color:#F00'>ETH��ǰ�۸��Ѿ�����%sRMB,��ע��Ͷ�ʷ���</div></p>" % rmbhigh
+            s = "<p><div style='color:#F00'>ETH当前价格已经超过%sRMB,请注意投资风险</div></p>" % rmbhigh
             con = "%s%s" % (con, s)
         if rmblow != 0 and rmb < rmblow:
-            s = "<p><div style='color:#00F'>ETH��ǰ�۸��Ѿ�����%sRMB,��ע��Ͷ�ʷ���</div></p>" % rmblow
+            s = "<p><div style='color:#00F'>ETH当前价格已经低于%sRMB,请注意投资风险</div></p>" % rmblow
             con = "%s%s" % (con, s)
         if usdthigt != 0 and huoBi > usdthigt:
-            s = "<p><div style='color:#F00'>ETH��ǰ�۸��Ѿ�����%sUSDT,��ע��Ͷ�ʷ���</div></p>" % usdthigt
+            s = "<p><div style='color:#F00'>ETH当前价格已经超过%sUSDT,请注意投资风险</div></p>" % usdthigt
             con = "%s%s" % (con, s)
         if usdtlow != 0 and huoBi < usdtlow:
-            s = "<p><div style='color:#00F'>ETH��ǰ�۸��Ѿ�����%sUSDT,��ע��Ͷ�ʷ���</div></p>" % usdtlow
+            s = "<p><div style='color:#00F'>ETH当前价格已经低于%sUSDT,请注意投资风险</div></p>" % usdtlow
             con = "%s%s" % (con, s)
         content = "<span>%s</span>" % con
-        title = "ETH��ǰ�۸�%sUSDTԼ%.2f�����" % (huoBi, rmb)
+        title = "ETH当前价格%sUSDT约%.2f人民币" % (huoBi, rmb)
         if len(email) > 0:
             EmailUtil.sendEmail(email, title, content, from_addr, qqCode)
         if len(send) > 0:
